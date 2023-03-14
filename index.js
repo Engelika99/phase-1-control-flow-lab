6a61fd14d0dd8326724f7379fe6beeb488581cbd
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-03-13 20:16:08 -0400

    Uses conditional statements

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(canRide){
   // Write your code here!
+  if (canRide <= 400){
+    return "This one is on me!"
+ }else if (canRide > 400 && canRide < 2000){
+  return "That will be twenty bucks."
+  }else if (canRide > 2000 && canRide < 2500){
+    return "I will gladly take your thirty bucks."
+  }else if (canRide >= 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  
+ return city <= "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  
+  
+  
+  switch(tip){
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return"Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+    }
 }
